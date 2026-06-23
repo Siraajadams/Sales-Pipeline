317ec11e21c14acd79e3fe189cc2d83b33b9a339
Siraajadams <siraaj.adams2003@gmail.com>
2026-06-23 23:06:21 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Sales Pipeline CRM",
+  description: "Videomed Commercial Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
